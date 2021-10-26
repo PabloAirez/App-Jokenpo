780d774d01f52704971ec30fa70836041566d68d
PabloAirez <pabloadscontato@gmail.com>
2021-10-25 22:48:41 -0300

    App funcionando!

--- app/src/main/java/com/example/pedra_papel_tesoura/MainActivity.java
+++ app/src/main/java/com/example/pedra_papel_tesoura/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -19,35 +20,45 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void selecionadoPedra(View view){
-        this.opcaoEscolhidaPeloUsuario(0);
+        this.mostrarResultado(0);
     }
 
     public void selecionadoPapel(View view){
-        this.opcaoEscolhidaPeloUsuario(1);
+        this.mostrarResultado(1);
     }
 
     public void selecionadoTesoura(View view){
-        this.opcaoEscolhidaPeloUsuario(2);
+        this.mostrarResultado(2);
     }
 
-    public void opcaoEscolhidaPeloUsuario(int opcao){
-        
+    public void mostrarResultado(int opcUsuario){
+        int app = this.opcaoDoAPP();
+        TextView resultado = findViewById(R.id.resultado);
+
+        if((app == 0 && opcUsuario == 1) || (app == 1 && opcUsuario == 2) || (app == 2 && opcUsuario == 0)){
+            resultado.setText("Você Venceu! :D");
+        } else if((app == 0 && opcUsuario == 0) || (app == 1 && opcUsuario == 1 ) || (app == 2 && opcUsuario == 2)){
+            resultado.setText("Empatou! :/");
+        } else{
+            resultado.setText("Você Perdeu :(");
+        }
     }
 
-    public void opcaoDoAPP(){
+    public int opcaoDoAPP(){
         int opcao = new Random().nextInt(3);
         ImageView apresentarResultado = findViewById(R.id.escolha);
 
         switch(opcao){
             case 0:
-                apresentarResultado.setImageResource(R.drawable.papel);
+                apresentarResultado.setImageResource(R.drawable.pedra);
              break;
             case 1:
-                apresentarResultado.setImageResource(R.drawable.pedra);
+                apresentarResultado.setImageResource(R.drawable.papel);
                 break;
             case 2:
                 apresentarResultado.setImageResource(R.drawable.tesoura);
                 break;
         }
+        return opcao;
     }
 }
